6bb2d007b81889431f30564e012a9ff83b1c65ec
winter-0101 <shinjiuuzaki@gmail.com>
2024-09-20 04:50:54 +0000

    9月19日分を別ファイルに移動

---
+++ 9_19/main.js
@@ -0,0 +1,17 @@
+document.querySelector("h1").textContent = "増田真志"
+document.querySelector("h2").textContent = "増田真志"
+
+//ドキュメントとは！！
+/*htmlファイルで書かれているコードを一つのオブジェクトとしてみなす。*/
+
+//DOM(ドム)
+/*ドキュメントオブジェクトモデル　（Document Object Model)　の略でwebページの要素やコンテンツなどをツリー構造で表現したデータモデル*/ 
+
+//上記コードの意味
+//document.querySelector("h1").textContent = "自分の名前"
+
+//ドキュメントの中の("h1")タグを探してね
+//document.querySelector("h1")
+
+//h1タグの中身のtextを変えてね
+//.textContent = "自分の名前"
