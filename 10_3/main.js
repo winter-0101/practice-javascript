befa5f769d5508187dfc6a47ddd61a25dc6b1554
winter-0101 <shinjiuuzaki@gmail.com>
2024-10-04 05:59:19 +0000

    10月４写経

---
+++ 10_3/main.js
@@ -0,0 +1,10 @@
+function errorInput(){
+console.log(document.getElementById("month").value)
+console.log(document.getElementById("day").value)
+
+const month = document.getElementById("month").value;
+const day = document.getElementById("day").value;
+
+console.log("month:",month,"day",day)
+
+}
