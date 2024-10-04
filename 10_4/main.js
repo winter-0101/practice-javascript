cfe369acf1035236e5f441d643960319533ee998
winter-0101 <shinjiuuzaki@gmail.com>
2024-10-04 06:16:53 +0000

    10月4日プルダウンリストの作成

--- 10_4/main.js
+++ 10_4/main.js
@@ -9,3 +9,14 @@ function errorInput(){
     }
     console.log("month:",month,"day:",day);
 }
+
+const monthSelect = document.getElementById("month2");
+monthSelectList();
+function monthSelectList(){
+    for (let i=1;i<12;i++){
+        let option = document.createElement('option');
+        option.value = i;
+        option.textContent = i + "月";
+        monthSelect.appendChild(option);
+    }
+}
