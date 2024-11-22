82736a853ee60b8de1bbdc368bc1092661014019
winter-0101 <shinjiuuzaki@gmail.com>
2024-11-22 06:14:55 +0000

    11月22

--- RPS.js
+++ RPS.js
@@ -43,7 +43,7 @@ function RPS(){
 
     let enemyImage = document.createElement("img");
         enemyImage.alt ="choice";
-        enemyImage.style.width ="200px";
+        enemyImage.style.width ="300px";
         enemyImage.style.height ="200px";
 
 
