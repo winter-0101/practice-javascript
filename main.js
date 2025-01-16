2d0d9a79746ab1d3747a52587f4ffa475e97c0f5
winter-0101 <shinjiuuzaki@gmail.com>
2025-01-16 05:20:53 +0000

    ボタンの追加

--- main.js
+++ main.js
@@ -8,12 +8,32 @@ function ClickCounterGame(){
     let counter = document.createElement("p");
     counter.textContent = count;
 
+    let button2 = document.createElement("button");
+    button2.textContent = "+10ボタン";
+
+    let button3 = document.createElement("button");
+    button3.textContent = "reset";
+
     button1.addEventListener("click",function(){
         count++;
         counter.textContent = count;
     })
 
+    button2.addEventListener("click",function(){
+        count = count + 10;
+        counter.textContent = count;
+    })
+
+    button3.addEventListener("click",function(){
+        count = 0;
+        counter.textContent = count;
+    })
+
+
+
     gameContainer.appendChild(button1);
+    gameContainer.appendChild(button2);
+    gameContainer.appendChild(button3);
     gameContainer.appendChild(counter);
 }
 ClickCounterGame();
